4927aa613d162866404e38b3dba13255a3d2eea0
Henry Hogan <40265229+HenryEffiong@users.noreply.github.com>
2023-12-08 11:26:11 +0100

    fan out

--- work-queues/receive/receive.go
+++ work-queues/receive/receive.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"log"
-	"time"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -23,24 +21,63 @@ func consumeAMQP() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
+	err = ch.ExchangeDeclare(
+		"logs",   // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
 	// To receive, we must declare the queue from which we're going to consume
 	q, err := ch.QueueDeclare(
-		"task-queue-2", // name
-		true,           // durable
-		false,          // delete when unused
-		false,          // exclusive
-		false,          // no-wait
-		nil,            // arguments
+		// "task-queue-2", // name
+		// true,           // durable
+		// false,          // delete when unused
+		// false,          // exclusive
+		// false,          // no-wait
+		// nil,            // arguments
+
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	// set the prefetch count
-	err = ch.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
+	// Bind the queue with an exchange
+	err = ch.QueueBind(
+		q.Name, // queue name
+		"",     // routing key
+		"logs", // exchange
+		false,
+		nil,
 	)
-	failOnError(err, "Failed to set QoS")
+	failOnError(err, "Failed to bind a queue")
+
+	// set the prefetch count
+	// err = ch.Qos(
+	// 	1,     // prefetch count
+	// 	0,     // prefetch size
+	// 	false, // global
+	// )
+	// failOnError(err, "Failed to set QoS")
+
+	// msgs, err := ch.Consume(
+	// 	q.Name, // queue
+	// 	"",     // consumer
+	// 	true,   // auto-ack
+	// 	false,  // exclusive
+	// 	false,  // no-local
+	// 	false,  // no-wait
+	// 	nil,    // args
+	// )
+	// failOnError(err, "Failed to register a consumer")
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
@@ -56,12 +93,13 @@ func consumeAMQP() {
 	var forever chan struct{}
 	go func() {
 		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
-			dotCount := bytes.Count(d.Body, []byte("."))
-			t := time.Duration(dotCount)
-			time.Sleep(t * time.Second)
-			log.Printf("Done")
-			d.Ack(false)
+			// log.Printf("Received a message: %s", d.Body)
+			// dotCount := bytes.Count(d.Body, []byte("."))
+			// t := time.Duration(dotCount)
+			// time.Sleep(t * time.Second)
+			// log.Printf("Done")
+			// d.Ack(false)
+			log.Printf(" [x] %s", d.Body)
 		}
 	}()
 
--- work-queues/send/send.go
+++ work-queues/send/send.go
@@ -23,16 +23,28 @@ func main() {
 
 	// consumeAMQP()
 
-	// To send, we must declare a queue for us to send to
-	q, err := ch.QueueDeclare(
-		"task-queue-2", // name
-		true,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+	// Create an exchange
+	err = ch.ExchangeDeclare(
+		"logs",   // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
 	)
-	failOnError(err, "Failed to declare a queue")
+	failOnError(err, "Failed to declare an exchange")
+
+	// To send, we must declare a queue for us to send to
+	// q, err := ch.QueueDeclare(
+	// 	"",    // name
+	// 	false, // durable
+	// 	false, // delete when unused
+	// 	true,  // exclusive
+	// 	false, // no-wait
+	// 	nil,   // arguments
+	// )
+	// failOnError(err, "Failed to declare a queue")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -40,14 +52,14 @@ func main() {
 	// publish a message to the queue
 	body := bodyFrom(os.Args)
 	err = ch.PublishWithContext(ctx,
-		"",     // exchange
-		q.Name, // routing key
+		"logs", // exchange
+		"",     // routing key
 		false,  // mandatory
 		false,  // immediate
 		amqp.Publishing{
-			DeliveryMode: amqp.Persistent,
-			ContentType:  "text/plain",
-			Body:         []byte(body),
+			// DeliveryMode: amqp.Persistent,
+			ContentType: "text/plain",
+			Body:        []byte(body),
 		})
 	failOnError(err, "Failed to publish a message")
 	log.Printf(" [x] Sent %s\n", body)
